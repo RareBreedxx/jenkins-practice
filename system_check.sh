392b7762471b0c351f51be1eba2de195dda8fd0d
Yinka Fadoju <o.fadoju@yahoo.com>
2025-12-16 18:33:52 -0500

    Fix shebang and line endings for Alpine

--- system_check.sh
+++ system_check.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/bin/sh
+
+echo "System check started..."
 echo "System check started...." > system_check.log
 echo "All good" >> system_check.log
-
+echo "System check completed successfully."
